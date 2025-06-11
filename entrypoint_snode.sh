e7a3b3410f47c20c040a31cdab0817f506c5be99
Shawn <44221603+shaspitz@users.noreply.github.com>
2025-06-11 15:57:55 -0700

    fix: use auth port

--- entrypoint_snode.sh
+++ entrypoint_snode.sh
@@ -3,7 +3,7 @@ set -eu
 
 exec snode leader \
   --instance-id "leader1" \
-  --eth-client-url "http://newgeth:8545" \
+  --eth-client-url "http://newgeth:8551" \
   --jwt-secret "13373d9a0257983ad150392d7ddb2f9172c9396b4c450e26af469d123c7aaa5c" \
   --priority-fee-recipient "0xfA0B0f5d298d28EFE4d35641724141ef19C05684" \
   --postgres-dsn "postgres://mevcommit:password123@postgres:5432/mevcommit?sslmode=disable" \
