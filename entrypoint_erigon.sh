a522f33930f6516d0c231d8c4b34d2728a10e6d0
Shawn <44221603+shaspitz@users.noreply.github.com>
2025-06-20 13:10:06 -0700

    works up to import

--- entrypoint_erigon.sh
+++ entrypoint_erigon.sh
@@ -9,27 +9,26 @@ jq ".config.shanghaiTime = $UPGRADE_TIMESTAMP_MS" /genesis.json > /genesis.json.
 jq ".config.cancunTime = $UPGRADE_TIMESTAMP_MS" /genesis.json > /genesis.json.tmp && mv /genesis.json.tmp /genesis.json
 jq ".config.pragueTime = $UPGRADE_TIMESTAMP_MS" /genesis.json > /genesis.json.tmp && mv /genesis.json.tmp /genesis.json
 
-echo "initializing newgeth"
-geth init --datadir /newgeth-data /genesis.json
+echo "initializing erigon"
+erigon init --datadir /erigon-data /genesis.json
 
 echo "importing chain.rlp"
-# Note we may want to do this import with multiple rlp files in case of failures
-geth import --datadir /newgeth-data /data/chain.rlp
+erigon import --datadir /erigon-data /data/chain.rlp
 
-echo "starting newgeth"
-exec geth --verbosity 5 \
-  --datadir /newgeth-data \
-  --nodiscover \
-  --http \
-  --http.port 8545 \
-  --http.addr 0.0.0.0 \
-  --http.api "admin,eth,net,web3,engine" \
-  --http.vhosts "*" \
-  --port 30303 \
+echo "starting erigon"
+exec erigon \
+  --datadir /erigon-data \
+  --externalcl \
   --authrpc.addr 0.0.0.0 \
   --authrpc.port 8551 \
-  --authrpc.vhosts "*" \
   --authrpc.jwtsecret /jwt.hex \
   --networkid 17864 \
-  --syncmode full \
-  --miner.recommit 900ms 
+  --bootnodes "" \
+  --nodiscover \
+  --http \
+  --http.api "eth,engine,erigon,web3" \
+  --http.port 8545 \
+  --http.addr 0.0.0.0 \
+  --private-api.addr=localhost:9090 \
+  --ws=false \
+  --verbosity info \
