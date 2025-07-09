d04896754b1137427d9d1f8b04974a8ee42a6e8e
Shawn <44221603+shaspitz@users.noreply.github.com>
2025-07-09 15:50:02 -0700

    wip

--- entrypoint_erigon.sh
+++ entrypoint_erigon.sh
@@ -3,17 +3,8 @@ set -eu
 
 echo "13373d9a0257983ad150392d7ddb2f9172c9396b4c450e26af469d123c7aaa5c" > /jwt.hex
 
-UPGRADE_TIMESTAMP_MS=$(cat /data/upgrade_timestamp_ms.txt)
-
-jq ".config.shanghaiTime = $UPGRADE_TIMESTAMP_MS" /genesis.json > /genesis.json.tmp && mv /genesis.json.tmp /genesis.json
-jq ".config.cancunTime = $UPGRADE_TIMESTAMP_MS" /genesis.json > /genesis.json.tmp && mv /genesis.json.tmp /genesis.json
-jq ".config.pragueTime = $UPGRADE_TIMESTAMP_MS" /genesis.json > /genesis.json.tmp && mv /genesis.json.tmp /genesis.json
-
 echo "initializing erigon"
 erigon init --datadir /erigon-data /genesis.json
-
-echo "importing chain.rlp"
-erigon import --datadir /erigon-data /data/chain.rlp
 
 echo "starting erigon"
 exec erigon \
@@ -22,6 +13,7 @@ exec erigon \
   --authrpc.addr 0.0.0.0 \
   --authrpc.port 8551 \
   --authrpc.jwtsecret /jwt.hex \
+  --authrpc.vhosts "*" \
   --networkid 17864 \
   --bootnodes "" \
   --nodiscover \
@@ -29,6 +21,6 @@ exec erigon \
   --http.api "eth,engine,erigon,web3" \
   --http.port 8545 \
   --http.addr 0.0.0.0 \
-  --private-api.addr=localhost:9090 \
+  --http.vhosts "*" \
   --ws=false \
   --verbosity info \
