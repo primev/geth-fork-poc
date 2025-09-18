258051cb50ea228e415f55265d4ab41c47f02f87
Shawn <44221603+shaspitz@users.noreply.github.com>
2025-09-17 23:12:09 -0700

    redis setup

--- entrypoint_follower.sh
+++ entrypoint_follower.sh
@@ -5,5 +5,5 @@ exec snode follower \
   --instance-id "follower1" \
   --eth-client-url "http://erigon-follower:8551" \
   --jwt-secret "13373d9a0257983ad150392d7ddb2f9172c9396b4c450e26af469d123c7aaa5c" \
-  --postgres-dsn "postgres://mevcommit:password123@postgres:5432/mevcommit?sslmode=disable" \
+  --redis-url "redis://redis:6379" \
   --log-level "debug" \
--- entrypoint_snode.sh
+++ entrypoint_snode.sh
@@ -7,7 +7,7 @@ exec snode leader \
   --eth-client-url "http://erigon:8551" \
   --jwt-secret "13373d9a0257983ad150392d7ddb2f9172c9396b4c450e26af469d123c7aaa5c" \
   --priority-fee-recipient "0xfA0B0f5d298d28EFE4d35641724141ef19C05684" \
-  --postgres-dsn "postgres://mevcommit:password123@postgres:5432/mevcommit?sslmode=disable" \
+  --redis-url "redis://redis:6379" \
   --evm-build-delay "30ms" \
   --evm-build-delay-empty-block "2s" \
   --api-addr ":9090" \
